1fb38be46d3962a9e223f271ca32581a6940de10
Kunal khowal <knlsaini100@gmail.com>
2024-03-22 15:46:38 +0530

    Delete control statements directory

--- control statements/if else.py
+++
@@ -1,5 +0,0 @@
-a=int(input("enter any value"))
-if(a<10):
-    print("a is greater")
-else:
-    print("a is smaller")
--- control statements/odd even.py
+++
@@ -1,5 +0,0 @@
-a=int(input("enter any number"))
-if(a%2==0):
-    print("number is even")
-else:
-    print("number is odd")
